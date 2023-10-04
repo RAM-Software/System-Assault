065524acbc34d7bc81afb4e1e83d67ddeb6558b8
hi5aj <93416051+hi5aj@users.noreply.github.com>
2023-10-04 17:15:30 -0400

    Added UI Hud
    - Updated SpawnUnit script
    - Added new scene with HUD

---
+++ Assets/Scripts/UI/SelectUnit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectUnit : MonoBehaviour
+{
+    public static string currentTroop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTroop = "Basic";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBasicTroop()
+    {
+        currentTroop = "Basic";
+        Debug.Log(currentTroop);
+    }
+    public void SetFastTroop()
+    {
+        currentTroop = "Fast";
+        Debug.Log(currentTroop);
+    }
+    public void SetTankTroop()
+    {
+        currentTroop = "Tank";
+        Debug.Log(currentTroop);
+    }
+
+}
--- Assets/Scripts/Units/PlaceUnit.cs
+++ Assets/Scripts/Units/PlaceUnit.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class PlaceUnit : MonoBehaviour, ITap
 {
-    public GameObject spawnUnit;
+    public GameObject normalUnit, fastUnit, tankUnit;
     public Transform spawnPosition;
     public Transform spawnUnitPosition;
+    public string activeTroop;
+
+    void Update()
+    {
+        activeTroop = SelectUnit.currentTroop;
+    }
     public void onTapAction()
     {
-        Instantiate(spawnUnit, spawnPosition.position, spawnUnitPosition.rotation);
+        if (activeTroop == "Basic")
+        {
+            Instantiate(normalUnit, spawnPosition.position, spawnUnitPosition.rotation);
+        }
+        if (activeTroop == "Fast")
+        {
+            Instantiate(fastUnit, spawnPosition.position, spawnUnitPosition.rotation);
+        }
+        if (activeTroop == "Tank")
+        {
+            Instantiate(tankUnit, spawnPosition.position, spawnUnitPosition.rotation);
+        }
     }
 }
