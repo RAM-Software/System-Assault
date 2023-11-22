375e3f9109dfd63002f4b996ea05a342938eea72
SheikhIslamM <SheikhIslamM@gmail.com>
2023-11-22 00:26:14 -0500

    Game mode Level and DLC Level changes + Adjustments
    - Reduced volume of tower firing sounds
    - Reduced volume of boss firing sounds
    - Changed ransomware boss firing sound
    - Removed some decorations from game mode level to reduce clutter
    - Created malware boss prefab
    - Added some decorations to DLC level

--- Assets/Scripts/Enemies/Base_Tower.cs
+++ Assets/Scripts/Enemies/Base_Tower.cs
@@ -92,8 +92,8 @@ public class Base_Tower : MonoBehaviour
         
         
         //Audio code
-        //PlaySound(towerShoot);
-        AudioManager.instance.PlaySFX("TowerShoot");
+        PlaySound(towerShoot);
+        //AudioManager.instance.PlaySFX("TowerShoot");
 
         if (projectile != null)
             projectile.Seek(target);
